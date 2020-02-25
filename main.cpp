dfa43cb963c5ee05a2abfe3341006d346f1a8ae6
ItsTheWoosh <52552359+ItsTheWoosh@users.noreply.github.com>
2020-02-25 15:18:37 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
+/**
+ * Aric Shi
+ * February 18, 2020
+ * Caesar 1: c = p + 3
+ */
+
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-    bool isLetter(char s) {
-        if ((s >= 65 && s <= 90) || (s >= 97 && s <= 122)) {
-            return true;
-        } else return false;
-    }
+bool isLetter(char symbol)
+{
+    if ((symbol >= 65 && symbol <= 90) || (symbol >= 97 && symbol <= 122))
+    {
+        return true;
+    } else return false;
+}
 
-    bool isXYZ (char s) {
-        if ((s >= 120 && s <= 122) || (s >= 88 && s <= 90)) {
-            return true;
-        } else return false;
-    }
 
-int main(){
+bool isXYZ (char symbol)
+{
+    if ((symbol >= 120 && symbol <= 122) || (symbol >= 88 && symbol <= 90))
+    {
+        return true;
+    } else return false;
+}
+
+int main()
+{
     ofstream outFile("Caesar1.txt");
     ifstream inFile("Caesar1.in");
 
@@ -23,13 +35,16 @@ int main(){
     char cypher;
     bool letter;
 
-    if (inFile.fail()) {
+    if (inFile.fail())
+    {
         cout << "Sorry, could not open file" << endl;
     }
 
-    while (inFile.get(plain)) {
+    while (inFile.get(plain))
+    {
         letter = isLetter(plain);
-        if (letter == true) {
+        if (letter == true)
+        {
             if (isXYZ(plain) == true) cypher = plain - 23;
             else                      cypher = plain + 3;
         } else cypher = plain;
